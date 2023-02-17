06ba73bd106d2cfb6f318e6465b42172dc301e01
ztraore <zie.traore@etudiant.univ-rennes1>
2023-02-17 10:20:36 +0100

    reponse 1 & 2

--- TP2/Manipulationdeslayouts/app/src/main/java/com/example/manipulationdeslayouts/ui/theme/Theme.kt
+++ TP2/Manipulationdeslayouts/app/src/main/java/com/example/manipulationdeslayouts/ui/theme/Theme.kt
@@ -28,10 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun ManipulationDesLayoutsTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
+fun ManipulationDesLayoutsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
