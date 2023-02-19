c3e7a505a38b9af701f165883c87d1a56bfa9b20
ztraore <zie.traore@etudiant.univ-rennes1>
2023-02-19 19:21:01 +0100

    reponse 6

--- TP2/Manipulationdeslayouts/app/src/main/java/com/example/manipulationdeslayouts/MainActivity.kt
+++ TP2/Manipulationdeslayouts/app/src/main/java/com/example/manipulationdeslayouts/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.manipulationdeslayouts
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.ComponentActivity
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_linear_layout)
+        setContentView(R.layout.main_departements_breton_layout)
+
+        // Création de la liste des départements bretons
+        val departementsBretons = arrayListOf("Côtes-d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan")
+
+        // Création de l'adaptateur pour la ListView
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, departementsBretons)
+        // Liaison de l'adaptateur avec la ListView
+        val listView: ListView = findViewById(R.id.my_listView)
+        listView.adapter = adapter
+
     }
 }
